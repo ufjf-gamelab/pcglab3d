982d6d27937ebbf82fb38420c28d613761a9a63d
BrunoDevMoraes <brunodevmoraes@gmail.com>
2025-12-10 19:10:49 -0300

    feat: adiciona animações ao player

--- scripts/playable/player.gd
+++ scripts/playable/player.gd
@@ -4,6 +4,12 @@ extends CharacterBody3D
 @export var gravity := 9.8
 @export var jump_force := 4.0
 
+var anim: AnimationPlayer
+
+func _ready():
+	anim = $Model/AnimationPlayer
+	anim.play("idle")
+
 func _physics_process(_delta):
 	var dir = Vector3.ZERO
 
@@ -18,8 +24,18 @@ func _physics_process(_delta):
 
 	if dir != Vector3.ZERO:
 		dir = dir.normalized()
+		velocity.x = dir.x * speed
+		velocity.z = dir.z * speed
 
-	velocity.x = dir.x * speed
-	velocity.z = dir.z * speed
+		if anim.current_animation != "walk":
+			anim.play("walk")
+
+		look_at(global_transform.origin + Vector3(dir.x, 0, dir.z), Vector3.UP)
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+		if anim.current_animation != "idle":
+			anim.play("idle")
 
 	move_and_slide()
